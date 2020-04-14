c173a1d8f432e19d86e67e4091e5301976d3942f
bmark88 <bradley.mark@hotmail.com>
2020-04-14 19:46:57 -0400

    update Appointment component and add Appointment Booked story

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
-function Appointment() {
+function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {(props.interview && <Show />)}
+      {(!props.interview && <Empty />)}
+    </article>
   )
 };
 
