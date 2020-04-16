161e900c0006a31d051bce37ff202ea29f1dec5c
bmark88 <bradley.mark@hotmail.com>
2020-04-16 19:27:44 -0400

    add SHOW, EMPTY, CREATE modes to Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,29 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode"
 
 function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {(props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer.name} />)}
-      {(!props.interview && <Empty />)}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+        />
+      )}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
     </article>
   )
 };
