d899d025e07ec54addfe911deecb1bae6efc9cbe
bmark88 <bradley.mark@hotmail.com>
2020-04-19 01:07:32 -0400

    add Appointment component with key=last to show 4pm-5pm time slot

--- src/components/Application.js
+++ src/components/Application.js
@@ -55,7 +55,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {schedule} 
+        {schedule}
+        <Appointment key="last" time="5pm" /> 
       </section>
     </main>
   );
