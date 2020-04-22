17a1f6c8a535886cb94cd27bb95c810b3ef9ded7
bmark88 <bradley.mark@hotmail.com>
2020-04-22 17:25:01 -0400

    Comment out SET_DAY case to test broken code

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -4,8 +4,8 @@ const SET_INTERVIEW = "SET_INTERVIEW";
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case SET_DAY:
-      return {...state, day: action.day};
+    // case SET_DAY:
+      // return {...state, day: action.day};
     case SET_APPLICATION_DATA:
       return {...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers};
     case SET_INTERVIEW: {
